f27f62a9336e71c5ebd061fd9dce972895dc207e
berkly <daddyrusher@gmail.com>
2018-11-09 17:02:55 +0500

    fix bugs

--- carfront/src/App.js
+++ carfront/src/App.js
@@ -1,17 +1,12 @@
 import React, {Component} from 'react';
-import CarList from "./components/CarList";
 import './App.css';
-import AppBar from "@material-ui/core/AppBar/AppBar";
-import Toolbar from "@material-ui/core/Toolbar/Toolbar";
+
 import Login from "./components/Login";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                {/*<AppBar position="static" color="default">
-                    <Toolbar><CarList/></Toolbar>
-                </AppBar>*/}
                 <Login/>
             </div>
         );
